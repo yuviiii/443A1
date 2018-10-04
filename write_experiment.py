3ff6b72985184ab99ec8d6e981439458bed12225
Ding YiTian <dingyiti@teach.cs.toronto.edu>
2018-10-04 15:35:46 -0400

    modified

--- write_experiment.py
+++ write_experiment.py
@@ -1,5 +1,5 @@
 import subprocess
 
-for byte_sizes in [100,2000,30000,80000,160000,900000,1500000,2000000,2500000,3000000]:
+for byte_sizes in [100,520,2018,8888,55555,120000,666666,1000000,2000000,3000000]:
 	tmp=subprocess.call(["./create_random_file", "mm", "104857600", str(byte_sizes)])
 
